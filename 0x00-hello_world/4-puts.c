11ad89bf6e5d47b3dd6df145ee29dd2d25816de7
iffekt <iffektstudio1@gmail.com>
2023-06-15 19:40:57 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
-
-
 /**
  * main - Entry point
- * 
+ *
  * Return: Always 0 (Success)
  */
 int main (void)
 {
-
-	puts ("\"Programming is like building a multi lingual puzzle\n");
+	puts("Programming is like building a multi lingual puzzle\n");
 	return (0);
-
 }
